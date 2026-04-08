3dad6030da093370bda49e342922da6b2257fd4a
Kaltham Ahashmi <s202371470@kfupm.edu.sa>
2026-04-08 13:03:51 +0300

    TODO1

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -105,9 +105,10 @@ LAB SETUP INSTRUCTIONS
  */
 
 //import express
-
+import express from "express";
 
 // create express app instance to create web server
+const app = express();
 
 
 
@@ -125,4 +126,11 @@ LAB SETUP INSTRUCTIONS
 
 // Start the server by listening
 
+app.get("/",(req, res) => {
+   res.send("Server up")
+});
+
+app.listen(3000, () => {
+   console.log("API running at http://localhost:3000");
+});
 
